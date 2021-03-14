ec7f0136ffd724094aad00fe06ca1540484570c8
tisin371@gmail.com <tisin371@gmail.com>
2021-03-14 20:45:45 +0300

    answer callback query

--- DemotivatorBot/BotMethods.cs
+++ DemotivatorBot/BotMethods.cs
@@ -471,6 +471,7 @@ namespace DemotivatorBot
                         );
                     break;
             }
+            await botClient.AnswerCallbackQueryAsync(callbackQueryEvent.CallbackQuery.Id);
         }
 
     }
